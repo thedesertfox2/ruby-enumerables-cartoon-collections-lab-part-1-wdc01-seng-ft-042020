f1b53f7dd2557bc22afd48cbe8c50647027a400a
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-14 14:33:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,9 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array
+  array.each do |array|
+    puts "Hello #{array}!"
+  end
   
 end
 
