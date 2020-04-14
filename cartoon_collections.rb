f2cad21661c853007367e37f9a6c291ae8edf7d8
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-14 15:51:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index {|item, index|  
-    puts "#{index} #{item}"
+    index = index + 1
+    puts " #{index} #{item}"
   }
   
   array
